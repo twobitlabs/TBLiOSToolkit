6fcf029c542dcdee62f7de8ffbaf8f4f0bfbff53
Christopher Pickslay <cpickslay@twobitlabs.com>
2016-02-18 09:28:31 -0800

    initial checkin

---
+++ TBLiOSToolkit/TBLiOSToolkit.h
@@ -0,0 +1,19 @@
+//
+//  TBLiOSToolkit.h
+//  TBLiOSToolkit
+//
+//  Created by Christopher Pickslay on 2/3/16.
+//  Copyright © 2016 Two Bit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TBLiOSToolkit.
+FOUNDATION_EXPORT double TBLiOSToolkitVersionNumber;
+
+//! Project version string for TBLiOSToolkit.
+FOUNDATION_EXPORT const unsigned char TBLiOSToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBLiOSToolkit/PublicHeader.h>
+
+
