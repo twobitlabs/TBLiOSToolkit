62d646905170f5d06a85b145cd798160ad009ebf
Christopher Pickslay <cpickslay@twobitlabs.com>
2016-03-03 14:48:44 -0800

    added GCDQueue syntactic sugar for GCD

---
+++ TBLiOSToolkit/GCDQueue.swift
@@ -0,0 +1,59 @@
+
+/**
+ Borrowed from http://nshipster.com/new-years-2016/ https://github.com/beeth0ven
+ 
+ Example usage:
+    GCDQueue.UserInitiated.executeAsync {
+        let url = NSURL(string: "http://image.jpg")!
+        let data = NSData(contentsOfURL: url)!
+        let image = UIImage(data: data)
+
+        GCDQueue.Main.executeAsync {
+            imageView.image = image
+        }
+    }
+ */
+
+protocol ExcutableQueue {
+    var queue: dispatch_queue_t { get }
+}
+
+extension ExcutableQueue {
+    func executeAsync(closure: () -> Void) {
+        dispatch_async(queue, closure)
+    }
+}
+
+enum GCDQueue: ExcutableQueue {
+    case Main
+    case UserInteractive
+    case UserInitiated
+    case Utility
+    case Background
+
+    var queue: dispatch_queue_t {
+        switch self {
+        case .Main:
+            return dispatch_get_main_queue()
+        case .UserInteractive:
+            return dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0)
+        case .UserInitiated:
+            return dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
+        case .Utility:
+            return dispatch_get_global_queue(QOS_CLASS_UTILITY, 0)
+        case .Background:
+            return dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)
+        }
+    }
+}
+
+enum SerialQueue: String, ExcutableQueue {
+    case DownLoadImage = "myApp.SerialQueue.DownLoadImage"
+    case UpLoadFile = "myApp.SerialQueue.UpLoadFile"
+
+    var queue: dispatch_queue_t {
+        return dispatch_queue_create(rawValue, DISPATCH_QUEUE_SERIAL)
+    }
+}
+
+
